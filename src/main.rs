81500060a36d7f1d9d846c2880a970640e28d17a
anchor-cable <forever.blue.moon@gmail.com>
2022-06-02 10:38:46 +0900

    done tutorial 1.2 : parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("pattern:{}", args.pattern);
+    println!("path:{:?}", args.path);
 }
